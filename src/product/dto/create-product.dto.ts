545ae1f3da6f952f0f99078ca1b3d5e0da450b86
MavitHieuDev <hieu102056@donga.edu.vn>
2025-05-26 23:14:50 +0700

    Update create-product.dto.ts

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -4,20 +4,3 @@ export class CreateProductDto {
   price!: number;
 }
 
-@Entity()
-export class Product {
-  @PrimaryGeneratedColumn()
-  id!: number;
-
-  @Column()
-  name!: string;
-
-  @Column('text')
-  description!: string;
-
-  @Column('decimal')
-  price!: number;
-
-  @Column({ default: true })
-  isActive!: boolean;
-}
