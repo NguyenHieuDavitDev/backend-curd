4389300e0d6547d1c6441281dd1f2a0cc4dc547f
MavitHieuDev <hieu102056@donga.edu.vn>
2025-05-26 23:22:46 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,33 +1,26 @@
 // api/index.ts
+
 import { createServer, proxy } from 'aws-serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
+import { Context, Handler } from 'aws-lambda';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express'; 
+import express from 'express';
 import { AppModule } from '../src/app.module';
 
-let cachedServer: Handler;
+let cachedServer: any;
 
-async function bootstrap(): Promise<Handler> {
-  // Tạo một instance Express
+async function bootstrapServer() {
   const expressApp = express();
-  // Khởi tạo Nest trên adapter Express
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
+
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
   await app.init();
-  // Tạo server Lambda proxy
+
   return createServer(expressApp);
 }
 
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
+export const handler: Handler = async (event, context) => {
   if (!cachedServer) {
-    cachedServer = await bootstrap();
+    cachedServer = await bootstrapServer();
   }
   return proxy(cachedServer, event, context, 'PROMISE').promise;
 };
